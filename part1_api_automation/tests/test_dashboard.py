61bd43e26289e34a43de4f541f130355916ef65f
sujin7373 <sujinsujin7733@gmail.com>
2026-02-05 17:09:48 +0900

    전체적으로 수정

--- part1_api_automation/tests/test_dashboard.py
+++ part1_api_automation/tests/test_dashboard.py
@@ -2,31 +2,31 @@ import pytest
 
 #---------------------------- 테스트 시나리오 1 ---------------------------------
 # STU-DAB-01-001
-def test_all_courses_success(dashboard_client,valid_headers,params):
-    endpoint = f"/student/{params['student_id']}"
+def test_all_courses_success(dashboard_client,valid_headers,dash_params):
+    endpoint = f"/student/{dash_params['student_id']}"
 
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["classroom_id"]
+            "classroom_id": dash_params["classroom_id"]
         }
     )
     body = response.json()
     assert response.status_code == 200, "Status code 오류"
-    assert body["account"]["id"] == int(params["student_id"]), "student_id가 올바르지 않습니다"
+    assert body["account"]["id"] == int(dash_params["student_id"]), "student_id가 올바르지 않습니다"
     assert len(body["learning_progress"]) > 0, "learning progress is not found"
    
 # STU-DAB-01-001 
-def test_all_courses_first_page(dashboard_client,valid_headers,params):
+def test_all_courses_first_page(dashboard_client,valid_headers,dash_params):
     valid_ids = {762843, 762844, 759075, 763817, 759074}
     
-    endpoint = f"/student/{params['student_id']}/course"
+    endpoint = f"/student/{dash_params['student_id']}/course"
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["classroom_id"],
+            "classroom_id": dash_params["classroom_id"],
             "offset" : 0,
             "count" : 5
         }
@@ -38,15 +38,15 @@ def test_all_courses_first_page(dashboard_client,valid_headers,params):
         assert course_id in valid_ids, f"Invalid course ID found: {course_id}"
     
 # STU-DAB-01-002
-def test_all_courses_second_page(dashboard_client,valid_headers,params):
+def test_all_courses_second_page(dashboard_client,valid_headers,dash_params):
     valid_ids = {764580, 764630, 764763, 764632, 765299}
     
-    endpoint = f"/student/{params['student_id']}/course"
+    endpoint = f"/student/{dash_params['student_id']}/course"
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["classroom_id"],
+            "classroom_id": dash_params["classroom_id"],
             "offset" : 5,
             "count" : 5
         }
@@ -58,14 +58,14 @@ def test_all_courses_second_page(dashboard_client,valid_headers,params):
         assert course_id in valid_ids, f"Invalid course ID found: {course_id}"
    
 # STU-DAB-01-003 
-def test_all_courses_invalid_header(dashboard_client,invalid_headers,params):
-    endpoint = f"/student/{params['student_id']}"
+def test_all_courses_invalid_header(dashboard_client,invalid_headers,dash_params):
+    endpoint = f"/student/{dash_params['student_id']}"
     
     response = dashboard_client.get(
         endpoint,
         headers = invalid_headers,
         params={
-            "classroom_id": params["classroom_id"]
+            "classroom_id": dash_params["classroom_id"]
         }
     )
     body = response.json()
@@ -73,14 +73,14 @@ def test_all_courses_invalid_header(dashboard_client,invalid_headers,params):
     assert "permission" in body["code"]
     
 # STU-DAB-01-004
-def test_all_courses_invalid_classroom(dashboard_client,valid_headers,params):
-    endpoint = f"/student/{params['student_id']}"
+def test_all_courses_invalid_classroom(dashboard_client,valid_headers,dash_params):
+    endpoint = f"/student/{dash_params['student_id']}"
 
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["invalid_classroom_id"]
+            "classroom_id": dash_params["invalid_classroom_id"]
         }
     )
     body = response.json()
@@ -88,14 +88,14 @@ def test_all_courses_invalid_classroom(dashboard_client,valid_headers,params):
     assert "model" in body["message"]
     
 # STU-DAB-01-005
-def test_all_courses_invalid_student(dashboard_client,valid_headers,params):
-    endpoint = f"/student/{params['invalid_student_id']}"
+def test_all_courses_invalid_student(dashboard_client,valid_headers,dash_params):
+    endpoint = f"/student/{dash_params['invalid_student_id']}"
 
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["classroom_id"]
+            "classroom_id": dash_params["classroom_id"]
         }
     )
     body = response.json()
@@ -103,8 +103,8 @@ def test_all_courses_invalid_student(dashboard_client,valid_headers,params):
     assert "model" in body["message"]
     
 # STU-DAB-01-006
-def test_all_courses_delete(classroom_client,valid_headers,params):
-    endpoint = f"/classroom/{params['classroom_id']}"
+def test_all_courses_delete(classroom_client,valid_headers,dash_params):
+    endpoint = f"/classroom/{dash_params['classroom_id']}"
 
     response = classroom_client.delete(
         endpoint,
@@ -116,15 +116,15 @@ def test_all_courses_delete(classroom_client,valid_headers,params):
     assert "permission" in body["code"]
     
 # STU-DAB-02-001
-def test_one_course(dashboard_client,valid_headers,params):
-    endpoint = f"/student/{params['student_id']}/lecture"
+def test_one_course(dashboard_client,valid_headers,dash_params):
+    endpoint = f"/student/{dash_params['student_id']}/lecture"
     
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["classroom_id"],
-            "course_id" : params["course_id"],
+            "classroom_id": dash_params["classroom_id"],
+            "course_id" : dash_params["course_id"],
             "filter_parent_lecture_id" : "null",
             "offset" : 0,
             "count" : 40
@@ -132,19 +132,19 @@ def test_one_course(dashboard_client,valid_headers,params):
     )
     body = response.json()
     assert response.status_code == 200
-    assert body[0]["lecture"]["course_id"] == params["course_id"]
+    assert body[0]["lecture"]["course_id"] == dash_params["course_id"]
     assert len(body[0]["learning_progress"]) > 0
     
 # STU-DAB-02-002
-def test_one_course_invalid_token(dashboard_client,invalid_headers,params):
-    endpoint = f"/student/{params['student_id']}/lecture"
+def test_one_course_invalid_token(dashboard_client,invalid_headers,dash_params):
+    endpoint = f"/student/{dash_params['student_id']}/lecture"
     
     response = dashboard_client.get(
         endpoint,
         headers = invalid_headers,
         params={
-            "classroom_id": params["classroom_id"],
-            "course_id" : params["course_id"],
+            "classroom_id": dash_params["classroom_id"],
+            "course_id" : dash_params["course_id"],
             "filter_parent_lecture_id" : "null",
             "offset" : 0,
             "count" : 40
@@ -155,15 +155,15 @@ def test_one_course_invalid_token(dashboard_client,invalid_headers,params):
     assert "permission" in body["message"]
     
 # STU-DAB-02-003
-def test_one_course_invalid_classroom(dashboard_client,valid_headers,params):
-    endpoint = f"/student/{params['student_id']}"
+def test_one_course_invalid_classroom(dashboard_client,valid_headers,dash_params):
+    endpoint = f"/student/{dash_params['student_id']}"
     
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["invalid_classroom_id"],
-            "course_id" : params["course_id"],
+            "classroom_id": dash_params["invalid_classroom_id"],
+            "course_id" : dash_params["course_id"],
         }
     )
     body = response.json()
@@ -171,15 +171,15 @@ def test_one_course_invalid_classroom(dashboard_client,valid_headers,params):
     assert "model" in body["message"]
     
 # STU-DAB-02-004
-def test_one_course_invalid_student(dashboard_client,valid_headers,params):
-    endpoint = f"/student/{params['invalid_student_id']}"
+def test_one_course_invalid_student(dashboard_client,valid_headers,dash_params):
+    endpoint = f"/student/{dash_params['invalid_student_id']}"
     
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["classroom_id"],
-            "course_id" : params["course_id"],
+            "classroom_id": dash_params["classroom_id"],
+            "course_id" : dash_params["course_id"],
         }
     )
     body = response.json()
@@ -187,15 +187,15 @@ def test_one_course_invalid_student(dashboard_client,valid_headers,params):
     assert "model" in body["message"]
     
 # STU-DAB-02-005
-def test_one_course_invalid_course(dashboard_client,valid_headers,params):
-    endpoint = f"/student/{params['student_id']}"
+def test_one_course_invalid_course(dashboard_client,valid_headers,dash_params):
+    endpoint = f"/student/{dash_params['student_id']}"
     
     response = dashboard_client.get(
         endpoint,
         headers = valid_headers,
         params={
-            "classroom_id": params["classroom_id"],
-            "course_id" : params["invalid_course_id"],
+            "classroom_id": dash_params["classroom_id"],
+            "course_id" : dash_params["invalid_course_id"],
         }
     )
     body = response.json()
