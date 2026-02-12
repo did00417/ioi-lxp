0808f12a2f95b95b5581f145e666157b00b4592f
DongBin <tlsehd6535@gmail.com>
2026-02-12 17:08:46 +0900

    동빈 테스트 완료

---
+++ part1_api_automation/ui-test/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def driver():
+    options = Options()
+    
+    options.add_argument("--start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+    yield driver
+    driver.quit()
